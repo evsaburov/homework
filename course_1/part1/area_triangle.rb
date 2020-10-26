b4aab1c3b3e5c4237b091981d7e4d604611b13d6
Evgeniy Craft <evs.zlat@gmail.com>
2020-10-26 20:23:07 +0500

    Заменил 1.0/2 на 0.5. Заменил to_i на to_f. Преобразование to_f в момент получения gets.chomp

--- course_1/part1/area_triangle.rb
+++ course_1/part1/area_triangle.rb
@@ -1,6 +1,6 @@
 puts "Введете основание(a) треугольника(см):"
-a = gets.chomp
+a = gets.chomp.to_f
 puts "Введите высоту(h) треугольника(см)"
-h = gets.chomp
-result = 1.0/2 * a.to_i * h.to_i
+h = gets.chomp.to_f
+result = 0.5 * a * h
 puts "Площадь треугольника равна #{result} см. кв."
