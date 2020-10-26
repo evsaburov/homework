85aa2c161e84b0f849486e89c96e4516c5ef6df5
Evgeniy Craft <evs.zlat@gmail.com>
2020-10-26 22:51:21 +0500

    Расставлены пробелы межу операторами. Вычисление корня в отдельной переменной. Использование методов zero,positive,negative.

--- course_1/part1/quad_equation.rb
+++ course_1/part1/quad_equation.rb
@@ -4,15 +4,16 @@ puts "Введите коэф. b:"
 b = gets.chomp.to_f
 puts "Введите коэф. c:"
 c = gets.chomp.to_f
-d = b**2-4*a*c
+d = b**2 - 4 * a * c
 puts "descr = #{d}"
-if(d==0)
-  x = -b*(2*a)
+if d.zero?
+  x = -b * (2 * a)
   puts "дискриминант D = 0, оба корня одинаковы a = b,x = #{x}"
-elsif (d<0)
+elsif d.negative?
   puts "Уравнение действительных решений не имеет"
-elsif (d>0)
-  x1 = (-b+Math.sqrt(d))/2*a
-  x2 = (-b-Math.sqrt(d))/2*a
+elsif d.positive?
+  msqrt = Math.sqrt(d)
+  x1 = (- b + msqrt) / 2 * a
+  x2 = (- b - msqrt) / 2 * a
   puts "Два корня, х1=#{x1.round(2)}, x2=#{x2.round(2)}"
 end
