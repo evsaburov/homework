789cba18248255672a23c4aa537e652ba290ec6b
Evgeniy Craft <evs.zlat@gmail.com>
2020-11-04 17:44:30 +0500

    add task6

---
+++ course_1/part2/task6.rb
@@ -0,0 +1,25 @@
+total = {}
+while true 
+  puts "Введите назавание товара:"
+  name = gets.chomp.to_s
+  break if name == 'стоп'
+  puts "Введите цену товара:"
+  price = gets.chomp.to_f.round(2)
+  puts "Введите количество товара:"
+  count = gets.chomp.to_i
+
+  total[name] = {price: price,count: count}
+end
+
+puts total
+
+toal_price = 0
+
+total.each { |k,v| 
+  full_price = v[:price] * v[:count]
+  puts "name: #{k}, full price:#{full_price}"
+  toal_price += full_price.round(2)
+
+}
+
+puts "total price: #{toal_price}" 
