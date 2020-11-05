d599ba7bb49c70a8083d1976cdf5f5d70b98428b
Evgeniy Craft <evs.zlat@gmail.com>
2020-11-05 23:01:24 +0500

    Исправляю заполнение массива, do для многострочных блоков, замена if на тернарный оператор

--- course_1/part2/task5.rb
+++ course_1/part2/task5.rb
@@ -5,26 +5,18 @@ month = gets.chomp.to_i
 puts "Enter year:"
 year = gets.chomp.to_i
 
+hyear = {1 => 31, 2 => 28, 3 => 31, 4 => 30, 5 => 31, 6 => 31, 7 => 31, 8 => 31, 9 => 31, 10 => 31, 11 => 31, 12 => 31}
 
 if (year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0
   puts "Високосный год"
-  hyear = {1 => 31, 2 => 29, 3 => 31, 4 => 30, 5 => 31, 6 => 31, 7 => 31, 8 => 31, 9 => 31, 10 => 31, 11 => 31, 12 => 31}
-else
-  puts "Не високосный год"
-  hyear = {1 => 31, 2 => 28, 3 => 31, 4 => 30, 5 => 31, 6 => 31, 7 => 31, 8 => 31, 9 => 31, 10 => 31, 11 => 31, 12 => 31}
+  hyear[2] = 29
 end
 
 i = 0
 
-hyear.each { |m,d|
-
- if m != month
-   i = i + d 
- else
-   i = i + day
- end
-
- break if m == month
-}
+hyear.each do |m,d|
+  i = (m != month) ? i + d : i + day
+  break if m == month
+end
 
 puts "Дата #{day}.#{month}.#{year} имеет порядковый номер - #{i}"
