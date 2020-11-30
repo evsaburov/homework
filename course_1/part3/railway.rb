54ed3a0e475c4041f6ce3297512568f5ac1cd1bc
Evgeniy Craft <evs.zlat@gmail.com>
2020-11-30 21:28:10 +0500

    fix all

--- course_1/part3/railway.rb
+++ course_1/part3/railway.rb
@@ -16,7 +16,7 @@ class Station
   end
 
   def get_trains_type
-    @trains.each { |train| puts "Номер поезда #{train.number}. Тип поезда #{train.type}. Количество вагонов #{train.count_carr}" }
+    @trains.each { |train| puts "Номер поезда #{train.number}. Тип поезда #{train.type}. Количество вагонов #{train.count_carriage}" }
   end
 
   def send_tranin(train)
@@ -35,11 +35,11 @@ class Route
     @midleS = []
   end
 
-  def add_mid_station(stationtion)
+  def add_station(stationtion)
     @midleS << stationtion
   end
 
-  def del_midleS(stationtion)
+  def del_station(stationtion)
     @midleS.delete(stationtion)
   end
 
@@ -48,8 +48,8 @@ class Route
   end
 
   def arr_route
-    arr_route = [] 
-    arr_route << @startS
+    routeS = [] 
+    routeS << @startS
     @midleS.each { |station| arr_route << station } 
     arr_route << @endS
   end
@@ -58,19 +58,19 @@ end
 
 class Train
   
-  attr_accessor :cur_station
+  attr_accessor :current_station
   attr_accessor :speed
   attr_accessor :route
   attr_reader :number
-  attr_reader :count_carr
+  attr_reader :count_carriage
   attr_reader :type
   
-  def initialize(number, type, count_carr)
-    @count_carr = count_carr
+  def initialize(number, type, count_carriage)
+    @count_carriage = count_carriage
     @number = number
     @type = type
-    @cur_station = nil
-    @route = nil
+    @current_station 
+    @route
     @speed = 0
   end
 
@@ -82,37 +82,39 @@ class Train
     @speed = 0
   end
 
-  def add_carr
-    @count_carr += 1 if @speed == 0
+  def stoped?
+    @speed.zero  
   end
 
-  def del_carr
-    @count_carr -= 1 if @speed == 0
+  def add_carriage
+    @count_carriage += 1 if self.stoped? 
+  end
+
+  def del_carriage
+    @count_carriage -= 1 if self.stoped?
   end
 
  def add_route(route)
    @route = route
-   @cur_station = route.startS
+   @current_station = route.startS
  end
 
  def next_station
-   station_index = @route.arr_route.index(@cur_station) 
-   @cur_station = @route.arr_route[station_index + 1] unless @route.arr_route[station_index + 1].nil?
+   station_index = @route.arr_route.index(@current_station) 
+   @current_station = @route.arr_route[station_index + 1] if @route.arr_route[station_index + 1]
  end 
 
  def back_station
-   station_index = @route.arr_route.index(@cur_station) 
-   @cur_station = @route.arr_route[station_index - 1] unless @route.arr_route[station_index - 1].nil?
+   station_index = @route.arr_route.index(@current_station) 
+   @current_station = @route.arr_route[station_index - 1] if @route.arr_route[station_index - 1]
  end 
 
  def location  
    arr_route = @route.arr_route
-   cur_index = arr_route.index(@cur_station)
-   puts "current station is #{arr_route[cur_index].name}" unless arr_route[cur_index].nil?
-   puts "last station is #{arr_route[cur_index - 1].name}" unless arr_route[cur_index - 1].nil?
-   puts "next station is #{arr_route[cur_index + 1].name}" unless arr_route[cur_index + 1].nil?
-
+   cur_index = arr_route.index(@current_station)
+   puts "current station is #{arr_route[cur_index].name}" if arr_route[cur_index]
+   puts "last station is #{arr_route[cur_index - 1].name}" if arr_route[cur_index - 1]
+   puts "next station is #{arr_route[cur_index + 1].name}" if arr_route[cur_index + 1]
  end
-
 end
 
