7dcd30ebcea1c3fc7f09ffead70f9aa20ea1d36b
evs.serv <evs.serv@gmail.com>
2021-01-17 15:24:40 +0500

    Отдельный метод, на каждый тип валидации с динамическим вызовом через метод valid_type.

--- course_1/part9/validation.rb
+++ course_1/part9/validation.rb
@@ -5,14 +5,23 @@ module Validation
   ERR_FORMAT = 'Значение атрибута не соответствует формату'
   ERR_TYPE = 'Значение атрибута не соответствует классу'
 
-  define_method("validate".to_sym) do |value, type_valid, params = nil|
-    if type_valid == :presence
-      raise ERR_PRESENCE if value.nil? or value.empty?
-    elsif  type_valid == :format
-      raise ERR_FORMAT unless value.match(params)
-    elsif  type_valid == :type
-      raise ERR_TYPE if value.is_a? params
-    end
+
+  def validate(value, type_valid, *params)
+    instance_eval("valid_#{type_valid}(value, *params)")
+  end
+
+  def valid_presence(value, *params)
+    raise ERR_PRESENCE if value.nil? || value.empty?
+  end
+
+  def valid_format(value, *params)
+    format = params[0]
+    raise ERR_FORMAT unless value.match(format)
+  end
+
+  def valid_type(value, *params)
+    tepe_object = params[0]
+    raise ERR_TYPE if value.is_a? tepe_object
   end
 
   def valid?
